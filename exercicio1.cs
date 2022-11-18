21d1b1cda11820588b454e194d9f832e6bf53c70
zroyyy <1zeussfrmdawic@gmail.com>
2022-11-18 12:12:42 -0300

    arquivo add

---
+++ exercicio1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] times = new string[5];
+            int[] pontos = new int [5];
+            int tp = 0;
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Digite o nome de um time de futebol");
+                times[i] = Console.ReadLine();
+                Console.WriteLine(" e quantos pontos o {0} tem", times[i]);
+                pontos[i] = int.Parse(Console.ReadLine());
+                tp++;
+            }
+        }
+    }
+}
