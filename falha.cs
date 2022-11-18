15d8f7770a03029706d0a4457d3a3c89e4f1bdab
zroyyy <1zeussfrmdawic@gmail.com>
2022-11-18 14:12:10 -0300

    n foi dessa vez kkkkk

---
+++ falha.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] times = new string[5];
+            int[] pontos = new int[5];
+            int tp = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Digite o nome de um time de futebol");
+                times[i] = Console.ReadLine();
+                Console.WriteLine(" e quantos pontos o {0} tem", times[i]);
+                pontos[i] = int.Parse(Console.ReadLine());
+                tp++;
+            }
+            for (int i = 0; i < tp; i++)
+            {
+                Console.WriteLine("{0} \t\t {1} pontos", times[i], pontos[i]);
+            }
+            Console.ReadKey();
+            {
+
+            }
+        }
+    }
+}
